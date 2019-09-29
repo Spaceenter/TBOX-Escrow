534d7c5a8834d29c971e35e4e98830c06083715f
William Apollo <4895865+Spaceenter@users.noreply.github.com>
2019-09-29 15:09:32 -0700

    Update CombinedEscrowAndToken.sol

--- CombinedEscrowAndToken.sol
+++ CombinedEscrowAndToken.sol
@@ -344,6 +344,5 @@ contract DACCToken is StandardToken {
     _totalSupply = 30 * (10 ** 9) * (10 ** uint256(decimals));
 
     _balances[msg.sender] = _totalSupply;
-    emit Transfer(0x0, msg.sender, _totalSupply);
   }
 }
